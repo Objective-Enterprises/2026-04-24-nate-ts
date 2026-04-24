40299753ce1cdf081658072f54aabad21f89afc8
David Y. Stephenson <david@davidystephenson.com>
2026-04-24 12:55:01 -0400

    Display literal types

---
+++ index.ts
@@ -0,0 +1,9 @@
+type Message = string | boolean
+type MyName = 'David' // literal type (The data must literally match a specific value)
+
+const message1: MyName = 'david'
+const message2: MyName = 'David'
+
+console.log(message1)
+console.log(message1)
+console.log(message1)
